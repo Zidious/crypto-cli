4a3cf6a391c0f2e0b4c5b47810ccbea47ac1853a
Zidious <41127686+Zidious@users.noreply.github.com>
2022-07-10 03:48:14 +0100

    chore: clean up and add README

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,22 @@ import { app } from './app';
 
 const cli = meow(
   `
-    Usage:
-    $ crypto 
+  Usage:
+  $ crypto --price <coin name> <additional flags>
 
-    Options:
-    --price, -p - Coin name
+  Options:
+  --price, --p - coin name
+  --priceChange, --pc - coin price change (%) in the past 24 hours
+  --volume, --v - coin volume in the past 24 hours
+  --high - highest price sold in the past 24 hours
+  --low - lowest price sold in the past 24 hours
+  --ath - coin all time high price
+  --athChange, --athc - percent price change from ATH
+  --version - current version of the crypto-cli tool
 
-    Examples:
-    $crypto --price bitcoin 
-    >> bitoin: $1337 
+  Examples:
+  $crypto --price bitcoin --pc
+  >> bitoin: $1337 - change (24H): 13.37%
 `,
   {
     flags: {
