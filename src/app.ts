5585c7c9bdaeccf5228e908c49f5fb0102127031
Gabe <41127686+Zidious@users.noreply.github.com>
2022-10-04 02:55:17 +0100

    chore: update dependencies (#36)

--- src/app.ts
+++ src/app.ts
@@ -15,35 +15,6 @@ interface Flags {
   save: string;
 }
 
-interface CoinMarketResponse {
-  id: string;
-  symbol: string;
-  name: string;
-  image: string;
-  current_price: number;
-  market_cap: number;
-  market_cap_rank: number;
-  fully_diluted_valuation: number;
-  total_volume: number;
-  high_24h: number;
-  low_24h: number;
-  price_change_24h: number;
-  price_change_percentage_24h: number;
-  market_cap_change_24h: number;
-  market_cap_change_percentage_24h: number;
-  circulating_supply: number;
-  total_supply: number;
-  max_supply: number;
-  ath: number;
-  ath_change_percentage: number;
-  ath_date: string;
-  atl: number;
-  atl_change_percentage: number;
-  atl_date: string;
-  roi: unknown;
-  last_updated: string;
-}
-
 export const app = async (action: string, flags: Record<string, unknown>) => {
   const { price, priceChange, volume, high, low, ath, athChange, save } =
     flags as unknown as Flags;
@@ -54,7 +25,7 @@ export const app = async (action: string, flags: Record<string, unknown>) => {
 
   const gecko = new CoinGeckoAPI();
   try {
-    const result: CoinMarketResponse[] = await gecko.coinMarkets({
+    const result = await gecko.coinMarkets({
       vs_currency: 'usd',
       ids: price.toString()
     });
