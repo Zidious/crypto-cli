84396435ba0fe5afc5032c3b79b29eea4f24755d
Michael <michaelsiek094@gmail.com>
2023-07-23 16:18:24 -0400

    fix!: use kebab casing (#59)

--- src/index.test.ts
+++ src/index.test.ts
@@ -96,13 +96,13 @@ describe('crypto-cli', () => {
       });
     });
 
-    describe('with `--priceChange, --pc`', () => {
+    describe('with `--price-change, --pc`', () => {
       describe('with a valid coin name', () => {
         it('returns the price change percentage', async () => {
           const results = await execa(CLI, [
             '--price',
             'bitcoin',
-            '--priceChange'
+            '--price-change'
           ]);
 
           assert.include(results.stdout, 'Bitcoin: $');
@@ -160,13 +160,13 @@ describe('crypto-cli', () => {
       });
     });
 
-    describe('with `--athChange`', () => {
+    describe('with `--ath-change`', () => {
       describe('with a valid coin name', () => {
         it('returns the percentage from the all time high price', async () => {
           const results = await execa(CLI, [
             '--price',
             'bitcoin',
-            '--athChange'
+            '--ath-change'
           ]);
 
           assert.include(results.stdout, 'Bitcoin: $');
