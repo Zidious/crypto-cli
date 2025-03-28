3ce0b7eae8a8efdccf1bb6bca9da5e948428d25a
Zidious <gaby1306@gmail.com>
2025-03-28 01:43:30 +0000

    fix: adjust price formatting to account for meme coins

--- src/utils.ts
+++ src/utils.ts
@@ -3,11 +3,6 @@ import chalk from 'chalk'
 const ERROR = chalk.bold.red
 const SUCCESS = chalk.bold.green
 
-const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
-})
-
 export const logError = (message: string) => {
   console.error(ERROR(message))
   process.exit(1)
@@ -18,7 +13,16 @@ export const logSuccess = (message: string) => {
 }
 
 export const format = (price: number) => {
-  return formatter.format(price)
+  const decimalPart = price.toString().split('.')[1]
+  // If the decimal part starts with 0000, it's likely a meme coin
+  // and we want to show more decimal places to avoid showing 0.00
+  const isMemeCoin = decimalPart && decimalPart.slice(0, 4) === '0000'
+
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: isMemeCoin ? 10 : 2
+  }).format(price)
 }
 
 export const formatFileName = (coinName: string, fileExt: string): string => {
